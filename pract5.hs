b2a3e951dd787b874e77d895d1f148b99a7f7c6c
Emma Jimenez <111333776+EmmaJimenez1604@users.noreply.github.com>
2022-10-29 19:22:54 -0500

    Update pract5.hs

--- pract5.hs
+++ pract5.hs
@@ -112,19 +112,19 @@ equiv_op (Impl a b) = (Disy (Neg ( (a))) ( (b)) )
 --equiv_op (Conj a b) = Conj ((a) ((b)))
 --equiv_op (Disy a b) = Disy (( (a)) ( (b)))
 
- 
-
 --dobleNeg 
 --Función que quita las dobles negaciones de una LProp.
 dobleNeg :: LProp -> LProp
-dobleNeg (Neg a)= Neg a
---dobleNeg (Neg(Neg a))= a
-dobleNeg PFalse=PFalse
-dobleNeg PTrue=PTrue
-dobleNeg (Conj (a)(b))=Conj(dobleNeg a)(dobleNeg b)
-dobleNeg (Disy (a)(b))=Disy(dobleNeg a)(dobleNeg b)
-dobleNeg (Impl (a)(b))=Impl(dobleNeg a)(dobleNeg b)
-dobleNeg (Syss (a)(b))=Syss(dobleNeg a)(dobleNeg b)
+dobleNeg PFalse = PFalse
+dobleNeg PTrue = PTrue
+dobleNeg (Var a)=Var a
+dobleNeg (Neg (Var a))= Neg (Var a)
+dobleNeg (Neg(Neg(Var a)))= Var a
+dobleNeg (Neg(Neg(Conj (Var a)(Var b))))= Conj(dobleNeg (Var a))(dobleNeg (Var b))
+{-dobleNeg (Conj (Var a)(Var b))=Conj(dobleNeg (Var a))(dobleNeg (Var b))
+dobleNeg (Disy (Var a)(Var b))=Disy(dobleNeg (Var a))(dobleNeg (Var b))
+dobleNeg (Impl (Var a)(Var b))=Impl(dobleNeg (Var a))(dobleNeg (Var b))
+dobleNeg (Syss (Var a)(Var b))=Syss(dobleNeg (Var a))(dobleNeg (Var b))-}
 
 --num_conectivos 
 --Función que redibe una LProp y contesta con el número de conectivos lógicos en la expresión.
